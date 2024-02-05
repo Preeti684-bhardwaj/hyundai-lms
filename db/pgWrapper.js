44e32ba9db6c91e8d58f607acc02db25910938c2
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2024-02-05 17:57:21 +0530

    vercel database

--- db/pgWrapper.js
+++ db/pgWrapper.js
@@ -1,12 +1,13 @@
 const Pool = require("pg").Pool;
 const dotenv= require("dotenv");
 dotenv.config();
+const {POSTGRES_USER,POSTGRES_HOST,POSTGRES_PASSWORD,POSTGRES_DATABASE} =process.env
 function query(queryString, cbFunc) {
   const pool = new Pool({
-    user:"chatbot_d8qc_user",
-    host:"dpg-cn0c5igcmk4c73am8u6g-a",
-    database:"chatbot_d8qc",
-    password:"DbCFjiM7OLFSvpXgafPHaqee0iDV8Zad",
+    user:POSTGRES_USER,
+    host:POSTGRES_HOST,
+    database:POSTGRES_DATABASE,
+    password:POSTGRES_PASSWORD,
     port: 5432,
   });
   pool.query(queryString, (error, results) => {
