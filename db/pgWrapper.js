a55c5683b85973ed00c6892df5b225020e7290cf
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2024-02-05 16:54:08 +0530

    second commit

--- db/pgWrapper.js
+++ db/pgWrapper.js
@@ -1,12 +1,12 @@
 const Pool = require("pg").Pool;
- dotenv= require("dotenv");
+const dotenv= require("dotenv");
 dotenv.config();
 function query(queryString, cbFunc) {
   const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "chatBot",
-    password: "987654",
+    user: process.env.USERNAME,
+    host: process.env.USERHOST,
+    database:  process.env.DATABASE,
+    password:  process.env.USERPASSWORD,
     port: 5432,
   });
   pool.query(queryString, (error, results) => {
