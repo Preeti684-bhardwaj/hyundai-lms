153703672630b9c9f66e4e4d85ab2c1712271c42
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2024-02-06 09:58:27 +0530

    status code added

--- auth/authenticator.js
+++ auth/authenticator.js
@@ -1,73 +1,67 @@
 let userDB;
 const {
-    isValidName,
-    isValidEmail,
-    isValidPassword,
-  } = require("../utils/validator");
-  
-module.exports = (injectedUserDB) => {
-    userDB = injectedUserDB;
+  isValidName,
+  isValidEmail,
+  isValidPassword,
+} = require("../utils/validator");
 
-    return {
-        registerUser,
-        login,
-    };
+module.exports = (injectedUserDB) => {
+  userDB = injectedUserDB;
+
+  return {
+    registerUser,
+    login,
+  };
 };
 
 function registerUser(req, res) {
-    userDB.isValidUser(req.body.username, (error, isValidUser) => {
-        if (error || !isValidUser) {
-            const message = error
-                ? "Something went wrong!"
-                : "This user already exists!";
-
-            sendResponse(res, message, error);
-
-            return;
-        }
-        if(!isValidEmail(req.body.username)){
-                const message = error
-                ? "Something went wrong!"
-                : "Email is invalid!";
-
-            sendResponse(res, message, error);
-
-            return;  
-        }
-        if(!isValidName(req.body.userTitle)){
-            const message = error
-            ? "Something went wrong!"
-            : "Name is not correct!";
-
-        sendResponse(res, message, error);
-
-        return;  
+  userDB.isValidUser(req.body.username, (error, isValidUser) => {
+    if (error || !isValidUser) {
+      sendResponse(res, 400, "Something went wrong!", error);
+      return;
     }
-    if(!isValidPassword(req.body.password)){
-        const message = error
-        ? "Something went wrong!"
-        : "Password should be strong, please use one number, one upper case, one lower case and one special character and characters should be between 8 to 15 only!";
 
-    sendResponse(res, message, error);
+    if (!isValidEmail(req.body.username)) {
+      sendResponse(res, 400, "Email is invalid!", error);
+      return;
+    }
 
-    return;  
-}
+    if (!isValidName(req.body.userTitle)) {
+      sendResponse(res, 400, "Name is not correct!", error);
+      return;
+    }
 
-        userDB.register(req.body.username, req.body.userTitle, req.body.password, (response) => {
-            sendResponse(
-                res,
-                response.error === undefined ? "Success!!" : "Something went wrong!",
-                response.error
-            );
-        });
-    });
+    if (!isValidPassword(req.body.password)) {
+      sendResponse(
+        res,
+        400,
+        "Password should be strong, please use one number, one upper case, one lower case, and one special character, and characters should be between 8 to 15 only!",
+        error
+      );
+      return;
+    }
+
+    userDB.register(
+      req.body.username,
+      req.body.userTitle,
+      req.body.password,
+      (response) => {
+        sendResponse(
+          res,
+          response.error === undefined ? 200 : 400,
+          response.error === undefined ? "Success!!" : "Something went wrong!",
+          response.error
+        );
+      }
+    );
+  });
 }
 
 function login(query, res) {}
 
-function sendResponse(res, message, error) {
-    res.status(error !== undefined ? 400 : 200).json({
-        message: message,
-        error: error,
-    });
+function sendResponse(res, statusCode, message, error) {
+  res.status(statusCode).json({
+    message: message,
+    error: error,
+  });
 }
