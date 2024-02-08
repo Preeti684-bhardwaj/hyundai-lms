e1118730267df0097e6168ff255f78cd8060002d
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2024-02-08 12:22:02 +0530

    RESET PASSWORD DONE

--- auth/routes.js
+++ auth/routes.js
@@ -2,7 +2,15 @@
 
 module.exports = (router, app, authenticator) => {
     router.post("/register", authenticator.registerUser);
-    router.post("/login", app.oauth.grant(), authenticator.login);
+    router.post("/login", (req, res, next) => {
+        app.oauth.grant()(req, res, (err) => {
+            if (err instanceof Error && err.code === 400 && err.name === 'OAuth2Error') {
+                // Handle invalid credentials error
+                return res.status(401).json({ error: 'Invalid username or password' });
+            }
+            next(err); // Pass other errors to the error handler middleware
+        });
+    }, authenticator.login);
     router.post("/forgot-password",authenticator.forgetPassword);
     router.post("/reset-password/:userId",authenticator.resetPassword);
     return router;
